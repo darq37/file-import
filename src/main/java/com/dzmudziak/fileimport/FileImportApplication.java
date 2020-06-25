3a37b7dac54fa1a1ba33006e20b75b148650370f
darq37 <dzmudziak@gmail.com>
2020-06-25 21:21:19 +0200

    Initial commit

---
+++ src/main/java/com/dzmudziak/fileimport/FileImportApplication.java
@@ -0,0 +1,13 @@
+package com.dzmudziak.fileimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileImportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dzmudziak/fileimport/FileImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzmudziak.fileimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
